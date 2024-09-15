8a9a9edd5c3ad8b21a954ebc7c5d0ec835f458d5
flydog555 <160736311+flydog555@users.noreply.github.com>
2024-09-15 21:59:07 +0800

    Add files via upload

---
+++ 选择和分支语句/分支语句ifelse/试验区.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int age = 10;
+//	if (age >= 18)
+//		printf("成年");
+//	else
+//		printf("未成年");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 200;
+//	if (age < 18)
+//		printf("少年\n");
+//	else if (age >= 18 && age <= 26)
+//		printf("青年\n");
+//	else if (age > 26 && age < 40)
+//		printf("中年\n");
+//	else
+//		printf("壮年\n");
+//	return 0;
+//}
+
+//判断一个数是否为奇数：
+//int main()
+//{
+//	int a = 0;
+//	printf("请输入一个整数：");
+//	scanf("%d", &a);
+//	if (a % 2 == 0)
+//	{
+//		printf("%d为偶数",a);
+//	}
+//	else
+//	{
+//		printf("%d为奇数",a);
+//	}
+//	return 0;
+//}
+
+//输出1-100之间的奇数
+//int main()
+//{
+//	for (int a = 1; a < 101; a++)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			printf("%d\n", a);
+//		}
+//	}
+//	return 0;
+//}
+//或者直接在for循环里a+=2
